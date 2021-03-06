c1e2b08878b8d88a9042f7079471d4c7f8a8714b
ayah <breakayah92@gmail.com>
2021-03-06 19:49:00 +0200

    getting up and running

---
+++ js.js
@@ -0,0 +1,11 @@
+'use strict';
+
+confirm("This is a guessing game, be ready my friend");
+alert("You should write Yes Or No and maybe Y Or N,"+" "+"be ready");
+let sum=0;
+let userName=prompt("Please provide your name");
+console.log(userName)
+
+alert("Hello"+" "+userName+ ","+"I hope you are fine, and welcome you in my guessing game.");
+ 
+// Ask 4 questions , Check the questions if empty,yes
